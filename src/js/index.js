04f557d3b9f5ff0ef119e832ed0b7ad60b9505c1
Francis <francis.lagares@gmail.com>
2019-09-13 12:07:03 +0200

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import 'bootstrap';
+import 'jquery';
+import 'tether';
+import 'popper.js';
+import 'bootstrap/js/dist/modal';
+import '../scss/styles.scss';
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'js/bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  mode: 'development',
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: false }
+          }
+        ]
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        loader: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: 'images'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts'
+            }
+          }
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/styles.css',
+    })
+  ]
+};
